022a2e38ab212d30ccd970f6c72214157f0fd8c0
Nigel Yoshida <tiepolo@sbcglobal.net>
2017-10-03 15:35:35 -0700

    Updated Ravenous React project.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,34 @@ import BusinessList from './components/BusinessList/BusinessList.js'
 import SearchBar from './components/SearchBar/SearchBar.js'
 import logo from './logo.svg';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList business={businesses} />
       </div>
     );
   }
