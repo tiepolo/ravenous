9ea45fc1a159aee2817d1251c65820b4069aa635
Nigel Yoshida <tiepolo@sbcglobal.net>
2017-10-03 15:50:31 -0700

    Fixed the callback function

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -8,7 +8,7 @@ class BusinessList extends React.Component {
       <div className="BusinessList">
         {
           this.props.businesses.map(function(business) {
-              <Business business={business} />;
+              <BusinessList business={businesses} />;
             }
           )
         }
