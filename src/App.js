86368fd1302d36502ce27703e22804fbacb360c3
Nigel Yoshida <tiepolo@sbcglobal.net>
2017-10-03 16:19:56 -0700

    Finished the second part of the Ravenous React project.

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList business={businesses} />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,8 +7,8 @@ class BusinessList extends React.Component {
     return (
       <div className="BusinessList">
         {
-          this.props.businesses.map(function(business) {
-              <BusinessList business={businesses} />;
+          this.props.businesses.map(business => {
+              return <Business business={business} key={business.id} />;
             }
           )
         }
