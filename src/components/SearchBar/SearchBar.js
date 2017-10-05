c796ff67cd2e17e0254ea6c920a5437c762a02c4
tiepolo <nigelyoshida@Nigels-iMac.local>
2017-10-05 07:22:56 -0700

    Commiting to work on remotely.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,20 +15,35 @@ class SearchBar extends React.Component {
       location: '',
       sortBy: 'best_match',
     };
-  }
-  renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
-    });
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
   }
 
-  getSortyByClass(sortByOption) {
+  getSortByClass(sortByOption) {
     if(this.state.sortBy === sortByOption) {
       return 'active';
     } else {
       return '';
     }
+  }
+
+  handleSortByChange(sortByOption) {
+    this.setState({sortBy: sortByOption});
+  }
+
+  handleTermChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  handleLocationChange(event) {
+    this.setState({location: event.target.value});
+  }
+
+  renderSortByOptions() {
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>{sortByOption}</li>;
+    });
   }
 
   render () {
@@ -40,8 +55,8 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange()} />
+          <input placeholder="Where?" onChange={this.handleLocationChange()} />
         </div>
         <div className="SearchBar-submit">
           <a>Let&#8217;s Go</a>
