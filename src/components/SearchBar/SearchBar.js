23978ebb6124308945a7feff5a3ebfc2d3a3012f
tiepolo <nigelyoshida@Nigels-iMac.local>
2017-10-04 19:08:53 -0700

    Uploading to get help.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,7 +7,23 @@ const sortByOptions = {
   'Most Reviewed': 'review_count'
 };
 
+function getSortyByClass(sortByOption) {
+  if(this.state.sortBy === sortByOption) {
+    return 'active';
+  } else {
+    return '';
+  }
+};
+
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match',
+    };
+  }
   renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOptions[sortByOption];
@@ -19,7 +35,7 @@ class SearchBar extends React.Component {
       <div className="SearchBar">
         <div className="SearchBar-sort-options">
           <ul>
-            renderSortByOptions();
+            {this.renderSortByOptions()}
           </ul>
         </div>
         <div className="SearchBar-fields">
